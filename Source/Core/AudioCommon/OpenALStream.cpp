2b19f940c0f08edbc17b543f99f316569633f544
LAGonauta <lagonauta@gmail.com>
2017-08-09 17:02:17 -0300

    Removed clamping and zeroing-out the LFE. They are not needed anymore.
    Also removed minimum number of frames needed when decoding DPL2.

--- Source/Core/AudioCommon/OpenALStream.cpp
+++ Source/Core/AudioCommon/OpenALStream.cpp
@@ -245,12 +245,6 @@ void OpenALStream::SoundLoop()
     frames_per_buffer = OAL_MAX_FRAMES;
   }
 
-  // DPL2 needs a minimum number of samples to work (FWRDURATION)
-  if (use_surround && frames_per_buffer < 240)
-  {
-    frames_per_buffer = 240;
-  }
-
   INFO_LOG(AUDIO, "Using %d buffers, each with %d audio frames for a total of %d.", OAL_BUFFERS,
            frames_per_buffer, frames_per_buffer * OAL_BUFFERS);
 
@@ -311,15 +305,6 @@ void OpenALStream::SoundLoop()
       if (rendered_frames < min_frames)
         continue;
 
-      // zero-out the subwoofer channel - DPL2Decode generates a pretty
-      // good 5.0 but not a good 5.1 output.  Sadly there is not a 5.0
-      // AL_FORMAT_50CHN32 to make this super-explicit.
-      // DPL2Decode output: LEFTFRONT, RIGHTFRONT, CENTREFRONT, (sub), LEFTREAR, RIGHTREAR
-      for (u32 i = 0; i < rendered_frames; ++i)
-      {
-        dpl2[i * SURROUND_CHANNELS + 3 /*sub/lfe*/] = 0.0f;
-      }
-
       if (float32_capable)
       {
         palBufferData(m_buffers[next_buffer], AL_FORMAT_51CHN32, dpl2.data(),
@@ -331,16 +316,7 @@ void OpenALStream::SoundLoop()
 
         for (u32 i = 0; i < rendered_frames * SURROUND_CHANNELS; ++i)
         {
-          // For some reason the ffdshow's DPL2 decoder outputs samples bigger than 1.
-          // Most are close to 2.5 and some go up to 8. Hard clamping here, we need to
-          // fix the decoder or implement a limiter.
-          dpl2[i] = dpl2[i] * (INT64_C(1) << 31);
-          if (dpl2[i] > INT_MAX)
-            surround_int32[i] = INT_MAX;
-          else if (dpl2[i] < INT_MIN)
-            surround_int32[i] = INT_MIN;
-          else
-            surround_int32[i] = static_cast<int>(dpl2[i]);
+          surround_int32[i] = static_cast<int>(dpl2[i] * std::numeric_limits<int>::max());
         }
 
         palBufferData(m_buffers[next_buffer], AL_FORMAT_51CHN32, surround_int32.data(),
@@ -352,13 +328,7 @@ void OpenALStream::SoundLoop()
 
         for (u32 i = 0; i < rendered_frames * SURROUND_CHANNELS; ++i)
         {
-          dpl2[i] = dpl2[i] * (1 << 15);
-          if (dpl2[i] > SHRT_MAX)
-            surround_short[i] = SHRT_MAX;
-          else if (dpl2[i] < SHRT_MIN)
-            surround_short[i] = SHRT_MIN;
-          else
-            surround_short[i] = static_cast<int>(dpl2[i]);
+          surround_short[i] = static_cast<int>(dpl2[i] * std::numeric_limits<short>::max());
         }
 
         palBufferData(m_buffers[next_buffer], AL_FORMAT_51CHN16, surround_short.data(),
